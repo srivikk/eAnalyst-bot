22494c59d704abb51562451758460d131bede7c4
Vikrant Srivastava <>
2021-10-06 16:56:46 +0530

    added crud for crud operation on data

--- controllers/crud-controller.js
+++ controllers/crud-controller.js
@@ -1,12 +1,25 @@
-const { readOperation } = require('./services/mongo-service/mongo-service')
+const { createOperation,updateOperation, deleteOperation, readOperation } = require('./services/mongo-service/mongo-service')
 
 exports.crudController = async (req, res) => {
     const collection = req.params.collection;
-    const body = req.body.name;
+    const data = req.body;
     const operation = req.query.operation
 
-    if (operation === "read") {
-        const data = await readOperation(collection,body)
+    if (operation === "create") {
+        await createOperation(collection, data)
+        console.log(data)
+        res.send('done!')
+    } else if (operation === "read") {
+        const data = await readOperation(collection)
         res.send(data)
+    } else if (operation === "update") {
+        await updateOperation(collection, data)
+        res.send('updated')
+    } else if (operation === "delete") {
+        await deleteOperation(collection, data)
+        res.send('deleted')
+    }
+    else{
+        res.send("Invalid operation")
     }
 }
--- controllers/services/mongo-service/mongo-service.js
+++ controllers/services/mongo-service/mongo-service.js
@@ -1,8 +1,26 @@
-const Supplier = require('../../../models/supplier')
+//Create
+exports.createOperation = async (colectionName, data) => {
+    const model = require("../../../models/" + colectionName)
+    const modelData = new model(data)
+    return await modelData.save();
+}
+
 //read
-exports.readOperation = async(collectionName,name) => {
-    const model = require("../../../models/" + collectionName)
-    console.log(name)
-    const data = await model.find({"name":name}).populate('supplier_id')
+exports.readOperation = async(colectionName) => {
+    const model = require("../../../models/" + colectionName)
+    const data = await model.find({})
     return data 
+}
+
+//update
+exports.updateOperation = async (colectionName,data) => {
+    const model = require("../../../models/" + colectionName)
+    await model.findOneAndUpdate(data.updatedBy, data.updateObject,{new:true})
+    return
+}
+
+exports.deleteOperation = async (colectionName,data) => {
+    const model = require("../../../models/" + colectionName)
+    await model.findOneAndRemove(data._id)
+    return 
 }
